9c3f8c8e816739fc66a5c01458d72f19be13fad8
shashub04 <shareefashubber04@gmail.com>
2025-10-18 20:25:08 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,2 @@
+document.getElementById("submit"). onsubmit= function (){alert("Your inquiry has been submitted"); return false;};
+document.getElementsByTagName("td")[0]. onclick= function (){alert("prices are subject to VAT");};
